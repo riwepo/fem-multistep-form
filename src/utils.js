2063a35a7c36010a33609750bc7bee11792744ac
Richard Potter <richardpotter.au@gmail.com>
2023-08-28 15:30:47 +1000

    put validation here

--- src/utils.js
+++ src/utils.js
@@ -44,3 +44,28 @@ export function getPrice(pricedItem, timeSpan) {
 export function getIcon(plan) {
   return `${process.env.PUBLIC_URL}/images/icon-${plan.name}.svg`;
 }
+
+export function validateEmail(email) {
+  if (
+    !email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    return "Please enter a valid email";
+  }
+  return "";
+}
+export function validatePhone(phone) {
+  if (
+    !phone.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)
+  ) {
+    return "Please enter a valid phone number";
+  }
+  return "";
+}
+export function validateName(name) {
+  if (!name.match(/^[a-zA-Z ]+$/)) {
+    return "Please enter a valid name";
+  }
+  return "";
+}
