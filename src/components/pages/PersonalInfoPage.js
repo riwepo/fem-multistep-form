9f74c61b11f57e374dfcc723c0859a6913690f51
Richard Potter <richardpotter.au@gmail.com>
2023-08-31 09:21:27 +1000

    implemented

--- src/components/pages/PersonalInfoPage.js
+++ src/components/pages/PersonalInfoPage.js
@@ -11,7 +11,6 @@ import StepControl from "../StepControl";
 function PersonalInfoPage() {
   return (
     <div className="personal-info-page flex bg-light-blue">
-      <div className="bg-image"></div>
       <ProgressIndicator steps={STEPS} activeStepCode={"PERSONAL_INFO"} />
       <PersonalInfo />
       <StepControl canGoBack={false} isValid={true} isLastPage={false} />
