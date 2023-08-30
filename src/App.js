9d89d9088736e995289f6a054d4a783376727257
Richard Potter <richardpotter.au@gmail.com>
2023-08-31 08:45:37 +1000

    reneder personal info page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DesignSystem from "./components/DesignSystem";
+import PersonalInfoPage from "./components/pages/PersonalInfoPage";
 
 function App() {
   return (
     <div className="App">
-      <DesignSystem />
+      <PersonalInfoPage />
+      {/* <DesignSystem /> */}
     </div>
   );
 }
