3b829f2da2c4898c5ef39202a9b6726a1077232b
Richard Potter <richardpotter.au@gmail.com>
2023-08-31 08:42:55 +1000

    started implement

---
+++ src/components/pages/PersonalInfoPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import PersonalInfo from "../PersonalInfo";
+import ProgressIndicator from "../ProgressIndicator";
+
+import { STEPS, getStepByCode } from "../../utils/steps";
+
+import "./PersonalInfoPage.css";
+import StepControl from "../StepControl";
+
+function PersonalInfoPage() {
+  return (
+    <div className="personal-info-page flex bg-light-blue">
+      <div className="bg-image"></div>
+      <ProgressIndicator steps={STEPS} activeStepCode={"PERSONAL_INFO"} />
+      <PersonalInfo />
+      <StepControl canGoBack={false} isValid={true} isLastPage={false} />
+    </div>
+  );
+}
+
+export default PersonalInfoPage;
