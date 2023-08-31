2b890b6e063b424d0d529eabeff7c868f2bceddb
Richard Potter <richardpotter.au@gmail.com>
2023-08-31 16:48:20 +1000

    defer to container

--- src/components/pages/PersonalInfoPage.js
+++ src/components/pages/PersonalInfoPage.js
@@ -1,28 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import PersonalInfo from "../PersonalInfo";
-import ProgressIndicator from "../ProgressIndicator";
 
-import { STEPS } from "../../utils/steps";
+import PageContainer from "./PageContainer";
+import { getStepByCode } from "../../utils/steps";
 
 import "./PersonalInfoPage.css";
-import StepControl from "../StepControl";
 
 function PersonalInfoPage() {
-  const [isPageValid, setIsPageValid] = useState(false);
-  const validChangeHandler = (state) => {
-    const isValid = Object.values(state).every((item) => item === true);
-    setIsPageValid(isValid);
-  };
   return (
-    <div className="personal-info-page grid">
-      <div className="progress-container">
-        <ProgressIndicator steps={STEPS} activeStepCode={"PERSONAL_INFO"} />
-      </div>
-
-      <PersonalInfo onValidChange={validChangeHandler} />
-      <StepControl canGoBack={false} isValid={isPageValid} isLastPage={false} />
-    </div>
+    <PageContainer step={getStepByCode("PERSONAL_INFO")}>
+      <PersonalInfo />
+    </PageContainer>
   );
 }
 
