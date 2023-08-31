1b46b5690833ded3538a18056a21ecef3d850664
Richard Potter <richardpotter.au@gmail.com>
2023-08-31 12:00:40 +1000

    placeholder for tests

---
+++ src/components/pages/PersonalInfoPage.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import PersonalInfoPage from "./PersonalInfoPage";
+
+test.skip("to do", () => {});
