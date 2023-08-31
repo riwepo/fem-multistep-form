56a1e2e8265e72520a3338f08bc19bd31daab71d
Richard Potter <richardpotter.au@gmail.com>
2023-08-31 16:48:39 +1000

    defer to container

--- src/components/pages/SelectPlanPage.js
+++ src/components/pages/SelectPlanPage.js
@@ -1,28 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import SelectPlan from "../SelectPlan";
-import ProgressIndicator from "../ProgressIndicator";
+import PageContainer from "./PageContainer";
 
-import { STEPS } from "../../utils/steps";
+import { getStepByCode } from "../../utils/steps";
 
 import "./SelectPlanPage.css";
-import StepControl from "../StepControl";
 
 function SelectPlanPage() {
-  const [isPageValid, setIsPageValid] = useState(false);
-  const validChangeHandler = (state) => {
-    const isValid = Object.values(state).every((item) => item === true);
-    setIsPageValid(isValid);
-  };
   return (
-    <div className="select-plan-page grid">
-      <div className="progress-container">
-        <ProgressIndicator steps={STEPS} activeStepCode={"SELECT_PLAN"} />
-      </div>
-
-      <SelectPlan onValidChange={validChangeHandler} />
-      <StepControl canGoBack={false} isValid={isPageValid} isLastPage={false} />
-    </div>
+    <PageContainer step={getStepByCode("SELECT_PLAN")}>
+      <SelectPlan />
+    </PageContainer>
   );
 }
 
