293cdbc93aef2428d900aa93fd0bf2f68103cddb
Richard Potter <richardpotter.au@gmail.com>
2023-08-31 15:37:10 +1000

    remove unnecessary inner div

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -11,20 +11,21 @@ function Plan({ plan, timeSpan, isActive, onActivated }) {
     onActivated && onActivated(plan.code);
   };
   return (
-    <Card className={`card-plan ${isActive ? "card-plan--active" : undefined}`}>
-      <div className="plan flex" onClick={clickHandler}>
-        <img src={getIconFilepath(plan)} alt="plan icon" />
-        <div>
-          <h2 className="text-marine-blue capitalize">{plan.name}</h2>
-          <p className="text-cool-grey">{getPriceDisplay(plan, timeSpan)}</p>
-          <p
-            className={`bonus ${
-              timeSpan.hasBonus ? "bonus--active" : undefined
-            } text-marine-blue`}
-          >
-            2 months free
-          </p>
-        </div>
+    <Card
+      className={`plan flex ${isActive ? "plan--active" : undefined}`}
+      onClick={clickHandler}
+    >
+      <img src={getIconFilepath(plan)} alt="plan icon" />
+      <div>
+        <h2 className="text-marine-blue capitalize">{plan.name}</h2>
+        <p className="text-cool-grey">{getPriceDisplay(plan, timeSpan)}</p>
+        <p
+          className={`bonus ${
+            timeSpan.hasBonus ? "bonus--active" : undefined
+          } text-marine-blue`}
+        >
+          2 months free
+        </p>
       </div>
     </Card>
   );
