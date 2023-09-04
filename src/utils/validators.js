b036f892511453a0ece2459b15c981790653ca0d
Richard Potter <richardpotter.au@gmail.com>
2023-09-04 11:07:02 +1000

    handle empty string

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,4 +1,7 @@
 export function validateEmail(email) {
+  if (!email) {
+    return "Please enter an email";
+  }
   if (
     !email.match(
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -9,6 +12,9 @@ export function validateEmail(email) {
   return "";
 }
 export function validatePhone(phone) {
+  if (!phone) {
+    return "Please enter a phone number";
+  }
   if (
     !phone.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)
   ) {
@@ -17,6 +23,9 @@ export function validatePhone(phone) {
   return "";
 }
 export function validateName(name) {
+  if (!name) {
+    return "Please enter a name";
+  }
   if (!name.match(/^[a-zA-Z ]+$/)) {
     return "Please enter a valid name";
   }
