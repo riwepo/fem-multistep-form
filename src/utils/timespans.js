e320d377110f0138ca6a1ea079551db37054045d
Richard Potter <richardpotter.au@gmail.com>
2023-09-05 12:11:15 +1000

    added name and short name

--- src/utils/timespans.js
+++ src/utils/timespans.js
@@ -1,8 +1,8 @@
 import { getItemByCode } from "./utils";
 
 export const TIME_SPANS = [
-  { id: 1, code: "MONTH", display: "mo", hasBonus: false },
-  { id: 2, code: "YEAR", display: "yr", hasBonus: true },
+  { id: 1, code: "MONTH", name: "monthly", shortName: "mo", hasBonus: false },
+  { id: 2, code: "YEAR", name: "yearly", shortName: "yr", hasBonus: true },
 ];
 
 export function getTimespanByCode(code) {
