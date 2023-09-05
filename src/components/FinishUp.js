e4ea8838db16b6996e3bbb1d90c07f5384b0301a
Richard Potter <richardpotter.au@gmail.com>
2023-09-05 12:11:44 +1000

    begain implement

--- src/components/FinishUp.js
+++ src/components/FinishUp.js
@@ -1,34 +1,54 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 
+import Card from "./Card";
 import StepCard from "./StepCard";
-import AddOn from "./AddOn";
 
 import { StepContext } from "../context/step-context";
 
-import { ADD_ONS } from "../utils/addOns";
+import { ADD_ONS, getAddOnByCode } from "../utils/addOns";
 import { getStepByCode } from "../utils/steps";
 
 import "./PickAddOns.css";
+import { getPlanByCode } from "../utils/plans";
+import { getPriceDisplay } from "../utils/utils";
 
-const STEP_CODE = "PICK_ADD_ONS";
+const STEP_CODE = "FINISH_UP";
+
+const selectedAddOnCodes = [ADD_ONS[0].code, ADD_ONS[1].code, ADD_ONS[2].code];
 
 function FinishUp({ timespan }) {
   const stepContext = useContext(StepContext);
-
-  // // on first render, set state of form from the context
-  // useEffect(() => {
-  //   const stepFieldState = stepContext.getStepFieldState(
-  //     STEP_CODE,
-  //     "selected_plan"
-  //   );
-  //   const _selectedPlan = stepFieldState.value;
-  // }, []);
+  const selectedPlanFieldState = stepContext.getStepFieldState(
+    "SELECT_PLAN",
+    "selected_plan"
+  );
+  const selectedPlanCode = selectedPlanFieldState.value;
+  const selectedPlan = getPlanByCode(selectedPlanCode);
 
   const finishUpStep = getStepByCode(STEP_CODE);
 
+  const displayAddOn = (addOnCode) => {
+    const addOn = getAddOnByCode(addOnCode);
+    return (
+      <div key={addOn.id} className="flex">
+        <p>{addOn.name}</p>
+        <p className="fw-bolder">{getPriceDisplay(addOn, timespan)}</p>
+      </div>
+    );
+  };
   return (
     <StepCard className="finish-up" stepInfo={finishUpStep}>
-      <h1>finish up</h1>
+      <Card className="container bg-light-gray text-marine-blue" onClick={null}>
+        <div className="flex">
+          <p className="fs-400 fw-bold text-marine-blue capitalize">{`${selectedPlan.name} (${timespan.name})`}</p>
+          <p className="fs-400 fw-bold text-marine-blue">
+            {getPriceDisplay(selectedPlan, timespan)}
+          </p>
+        </div>
+        {selectedAddOnCodes.map((addOnCode) => {
+          return displayAddOn(addOnCode);
+        })}
+      </Card>
     </StepCard>
   );
 }
