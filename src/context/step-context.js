0615b897189838915fdb1c9d095cfb8fa9f94038
Richard Potter <richardpotter.au@gmail.com>
2023-09-06 10:37:45 +1000

    re-worked step context

--- src/context/step-context.js
+++ src/context/step-context.js
@@ -2,24 +2,46 @@ import React, { useState } from "react";
 
 import { getItemByCode, replaceItemByCode } from "../utils/utils";
 
-import { STEPS } from "../utils/steps";
+import { ADD_ONS } from "../utils/addOns";
+import { FIELDS } from "../utils/personal_info";
 
-const defaultStepStates = STEPS.map((step) => ({
-  code: step.code,
-  fieldStates: step.fieldCodes.map((code) => ({
-    code: code,
-    value: "",
-    isValid: step.defaultFieldValid,
+const personalInfoCodes = FIELDS.map((field) => field.code);
+const addOnCodes = ADD_ONS.map((addOn) => addOn.code);
+
+const defaultStepStates = {
+  PERSONAL_INFO: {
+    state: personalInfoCodes.map((code) => ({
+      code: code,
+      value: "",
+      isValid: false,
+    })),
+    isValid: false,
     isInitialised: false,
-  })),
-}));
+  },
+  SELECT_PLAN: { state: "", isValid: false, isInitialised: false },
+  PICK_ADD_ONS: {
+    state: addOnCodes.map((code) => ({ code: code, isSelected: false })),
+    isValid: true,
+    isInitialised: false,
+  },
+};
 
 export const StepContext = React.createContext({
   stepStates: defaultStepStates,
 
-  setStepFieldState: (stepCode, fieldCode, fieldValue, isValid) => {},
+  setPersonalInfoField: (fieldCode, fieldValue, isValid) => {},
 
-  getStepFieldState: (stepCode, fieldCode) => {},
+  getPersonalInfoField: (fieldCode) => {},
+
+  setSelectedPlanCode(planCode) {},
+
+  getSelectedPlanCode() {},
+
+  selectAddOn(addOnCode, isSelected) {},
+
+  getAddOn(addOnCode) {},
+
+  isStepInitialised: (stepCode) => {},
 
   isStepValid: (stepCode) => {},
 });
@@ -27,40 +49,96 @@ export const StepContext = React.createContext({
 export default function StepContextProvider({ children }) {
   const [stepStates, setStepStates] = useState(defaultStepStates);
 
-  const _getStepFieldState = (stepCode, fieldCode) => {
-    const step = getItemByCode(stepStates, stepCode);
-    const fieldState = getItemByCode(step.fieldStates, fieldCode);
-    return fieldState;
-  };
-
-  const _setStepFieldState = (stepCode, fieldCode, fieldValue, isValid) => {
+  const _setPersonalInfoField = (fieldCode, fieldValue, isValid) => {
     setStepStates((currentStepStates) => {
-      const step = getItemByCode(currentStepStates, stepCode);
       const newFieldState = {
         code: fieldCode,
         value: fieldValue,
         isValid: isValid,
-        isInitialised: true,
       };
-      const newFieldStates = replaceItemByCode(step.fieldStates, newFieldState);
-      const newStepState = { code: stepCode, fieldStates: newFieldStates };
-      const newStepStates = replaceItemByCode(currentStepStates, newStepState);
+      const newState = replaceItemByCode(
+        currentStepStates.PERSONAL_INFO.state,
+        newFieldState
+      );
+      const newStepStates = {
+        ...currentStepStates,
+        PERSONAL_INFO: {
+          state: newState,
+          isValid: newState.every((field) => field.isValid),
+          isInitialised: true,
+        },
+      };
       return newStepStates;
     });
   };
 
-  const _isStepValid = (stepCode) => {
-    const step = getItemByCode(stepStates, stepCode);
-    const result = step.fieldStates.every((fieldState) => fieldState.isValid);
-    return result;
+  const _getPersonalInfoField = (fieldCode) => {
+    const fieldState = getItemByCode(stepStates.PERSONAL_INFO.state, fieldCode);
+    return fieldState;
   };
 
+  const _setSelectedPlanCode = (planCode) => {
+    setStepStates((currentStepStates) => {
+      const newStepStates = {
+        ...currentStepStates,
+        SELECT_PLAN: {
+          state: planCode,
+          isValid: planCode !== "",
+          isInitialised: true,
+        },
+      };
+      return newStepStates;
+    });
+  };
+
+  const _getSelectedPlanCode = () => {
+    return stepStates.SELECT_PLAN.state;
+  };
+
+  const _selectAddOn = (addOnCode, isSelected) => {
+    setStepStates((currentStepStates) => {
+      const newAddOn = {
+        code: addOnCode,
+        isSelected: isSelected,
+      };
+      const newAddOns = replaceItemByCode(
+        currentStepStates.PICK_ADD_ONS.state,
+        newAddOn
+      );
+      const newStepStates = {
+        ...currentStepStates,
+        PICK_ADD_ONS: {
+          state: newAddOns,
+          isValid: true,
+          isInitialised: true,
+        },
+      };
+      return newStepStates;
+    });
+  };
+
+  const _getAddOn = (code) => {
+    return getItemByCode(stepStates.PICK_ADD_ONS.state, code);
+  };
+
+  const _isStepInitialised = (stepCode) => {
+    return stepStates[stepCode].isInitialised;
+  };
+
+  const _isStepValid = (stepCode) => {
+    return stepStates[stepCode].isValid;
+  };
   return (
     <StepContext.Provider
       value={{
         stepStates: defaultStepStates,
-        getStepFieldState: _getStepFieldState,
-        setStepFieldState: _setStepFieldState,
+        setPersonalInfoField: _setPersonalInfoField,
+        getPersonalInfoField: _getPersonalInfoField,
+        setSelectedPlanCode: _setSelectedPlanCode,
+        getSelectedPlanCode: _getSelectedPlanCode,
+        selectAddOn: _selectAddOn,
+        getAddOn: _getAddOn,
+        isStepInitialised: _isStepInitialised,
         isStepValid: _isStepValid,
       }}
     >
