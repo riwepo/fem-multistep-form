fb66ef5fe618807e0cb8909f28ea85d417f54ca2
Richard Potter <richardpotter.au@gmail.com>
2023-09-07 10:35:53 +1000

    blur buttons after click

--- src/components/StepControl.js
+++ src/components/StepControl.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import "./StepControl.css";
 
@@ -9,20 +9,33 @@ function StepControl({
   onBackClicked,
   onFwdClicked,
 }) {
+  const backButtonRef = useRef(null);
+  const fwdButtonRef = useRef(null);
+
+  const backClickHandler = () => {
+    backButtonRef.current.blur();
+    onBackClicked();
+  };
+  const fwdClickHandler = () => {
+    fwdButtonRef.current.blur();
+    onFwdClicked();
+  };
   return (
     <div className="step-control flex">
       <button
         className={`btn btn-back ${
           canGoBack ? "btn-back--visible" : undefined
         }`}
-        onClick={onBackClicked}
+        onClick={backClickHandler}
+        ref={backButtonRef}
       >
         Go back
       </button>
       <button
         className={`btn btn-next ${isConfirm ? "btn-next--last" : undefined}`}
         disabled={!isValid}
-        onClick={onFwdClicked}
+        onClick={fwdClickHandler}
+        ref={fwdButtonRef}
       >
         {isConfirm ? "Confirm" : "Next step"}
       </button>
