8f365fc5cda974b95792d5b16209ef0ede722e15
Richard Potter <richardpotter.au@gmail.com>
2023-09-07 10:07:10 +1000

    implement and test autoFocus

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./Card.css";
 
-function Card({ className, onClick, children }) {
+function Card({ className, onClick, autoFocus, children }) {
+  const divRef = useRef(null);
+  useEffect(() => {
+    if (autoFocus && divRef.current) {
+      divRef.current.focus();
+    }
+  }, []);
   return (
-    <div className={`${className} card`} onClick={onClick}>
+    <div
+      className={`${className} card`}
+      ref={divRef}
+      onClick={onClick}
+      tabIndex={0} // this is needed to get focus
+    >
       {children}
     </div>
   );
--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -7,11 +7,33 @@ describe("card test suite", () => {
   test("renders Card content", () => {
     const text = "hello world";
     render(
-      <Card className={undefined} onClick={null}>
+      <Card className={undefined} autoFocus={false} onClick={null}>
         <h1>{text}</h1>
       </Card>
     );
     const h1Element = screen.getByText(text);
     expect(h1Element).toBeInTheDocument();
+  });
+
+  test("autoFocus false", () => {
+    const text = "hello world";
+    render(
+      <Card className={undefined} autoFocus={false} onClick={null}>
+        <h1>{text}</h1>
+      </Card>
+    );
+    const h1Element = screen.getByText(text);
+    expect(h1Element).not.toHaveFocus();
+  });
+
+  test("autoFocus true", () => {
+    const text = "hello world";
+    const { container } = render(
+      <Card className={undefined} autoFocus={true} onClick={null}>
+        <h1>{text}</h1>
+      </Card>
+    );
+    const cardDivElement = container.querySelector(".card"); // eslint-disable-line
+    expect(cardDivElement).toHaveFocus();
   });
 });
